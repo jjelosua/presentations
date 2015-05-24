4f5b99c94c303bf3f660692d95f546216654c909
Juan Elosua <juan.elosua@gmail.com>
2015-05-24 17:12:29 -0300

    Add local server config and modify paths

---
+++ server/README.md
@@ -0,0 +1,20 @@
+Local web server
+================
+
+## Requirements
+* Have [_nodejs_](https://nodejs.org/)
+
+## Run local
+1. Go to the server folder
+
+        $ cd server
+
+2. Install dependencies
+
+        $ npm install
+
+3. To launch the local server with live reload
+
+        $ gulp server
+
+4. Open a browser on **http://localhost:8080** and check that everything looks fine
---
+++ server/gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    jshint = require('gulp-jshint'),
+    watch = require('gulp-watch'),
+    stylish = require('jshint-stylish');
+
+gulp.task('lintHTML', function() {
+  return gulp.src('**/*.html',{ cwd: '../2015'})
+    // if flag is not defined default value is 'auto' 
+    .pipe(jshint.extract('auto'))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter(stylish));
+});
+
+// SERVER TASKS
+gulp.task('reload', function() {
+    gulp.src('../2015/**/*.html', {read: true })
+        .pipe(watch('../2015/**/*.html'))
+        .pipe(jshint.extract('auto'))
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter(stylish))
+        .pipe(connect.reload());
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '../',
+    livereload: true,
+    port:8080
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src('../2015/**/*.html')
+    .pipe(connect.reload());
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch(['../2015/**/*.html', 
+              '../2015/**/*.md',
+              '../2015/**/*.css',
+              '../2015/**/*.js',
+              '../2015/**/*.png',
+              '../2015/**/*.jpg'], ['html','lintHTML']);
+});
+
+// development server reload modified files
+gulp.task('server', ['connect', 'watch']);
+
+// default task
+gulp.task('default', function() {
+  console.log("D3 training at La Nacion");
+});
